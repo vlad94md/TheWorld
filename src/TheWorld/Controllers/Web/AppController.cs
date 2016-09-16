08d3d07243b086554a4a38e7f729cccf96266cce
vlad94md <i22vladg@mail.ru>
2016-09-16 09:26:12 +0300

    Start commit

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel viewModel)
+        {
+
+            _mailService.SendEmail("testemail@mail.com", viewModel.Email, viewModel.Name, viewModel.Message);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TheWorld/wwwroot/js/app.js
@@ -0,0 +1,17 @@
+﻿// ap.js
+(function start() {
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#toggleMenu").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        var $icon = $("#toggleMenu i.fa");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-chevron-left");
+            $icon.addClass("fa-chevron-right");
+        } else {
+            $icon.removeClass("fa-chevron-right");
+            $icon.addClass("fa-chevron-left");
+        }
+    });
+})();
