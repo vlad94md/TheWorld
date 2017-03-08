79d75d7e3facf01119dc9b4ccd2c43beaedd0678
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-03-08 23:05:30 +0200

    Added gulp and npm

---
+++ src/TheWorld/wwwroot/lib/_app/app.js
@@ -0,0 +1,1 @@
+!function(){var e=$("#sidebar, #wrapper");$("#toggleMenu").on("click",function(){e.toggleClass("hide-sidebar");var a=$("#toggleMenu i.fa");e.hasClass("hide-sidebar")?(a.removeClass("fa-chevron-left"),a.addClass("fa-chevron-right")):(a.removeClass("fa-chevron-right"),a.addClass("fa-chevron-left"))})}();
